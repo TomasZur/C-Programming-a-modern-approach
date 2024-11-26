e5fbe1b7116b33253785d315fa3cbd8f58b93a6f
Tomas Žuromskas <ttzuromskas@gmail.com>
2024-11-26 12:09:21 +0200

    10 Skyrius done

--- 10 Chapter/projects/project7test.c
+++ 10 Chapter/projects/project7test.c
@@ -12,46 +12,18 @@ char digits[3][4 * MAX_DIGITS];
 
 int main()
 {
-
-    int digit,position=0;
-    char ch;
-    clear_digits_array();
-
-    printf("Enter a number: ");
-   
-   do
-    {
-        ch = getchar();
-        if (isdigit(ch))
-        {
-            digit=ch-'0';
-            process_digit(digit,position);
-            position++;
-        }
-        
-
-    } while (ch != '\n');
-
-    print_digits_array();
-    return 0;
-}
-
-void clear_digits_array(void)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < (MAX_DIGITS * 4); j++)
-        {
-            digits[i][j] = ' ';
-        }
-    }
-}
-
-void process_digit(int digit, int position)
-{
-
-    int i = 0;
     
+    
+    int digit = 9;
+    int i = 0;
+    int j = 0;
+    for(i=0;i<3;i++){
+        for(j=0;j<(MAX_DIGITS*4);j++){
+            digits[i][j]='.';
+        }
+        printf("\n");
+    }
+
     for (i = 0; i < 7; i++)
     {
 
@@ -59,41 +31,40 @@ void process_digit(int digit, int position)
         {
             if (i == 0)
             {
-                digits[0][(position*4)+1] = '_';
+                digits[0][1] = '_';
             }
             else if (i == 1)
             {
-                digits[1][(position*4)+2] = '|';
+                digits[1][2] = '|';
             }
             else if (i == 2)
             {
-                digits[2][(position*4)+2] = '|';
+                digits[2][2] = '|';
             }
             else if (i == 3)
             {
-                digits[2][(position*4)+1] = '_';
+                digits[2][1] = '_';
             }
             else if (i == 4)
             {
-                digits[2][(position*4)+0] = '|';
+                digits[2][0] = '|';
             }
             else if (i == 5)
             {
-                digits[1][(position*4)+0] = '|';
+                digits[1][0] = '|';
             }
             else if (i == 6)
             {
-                digits[1][(position*4)+1] = '_';
+                digits[1][1] = '_';
             }
         }
     }
-}
-void print_digits_array(void){
-    int i,j;
     for(i=0;i<3;i++){
         for(j=0;j<(MAX_DIGITS*4);j++){
             printf("%c",digits[i][j]);
         }
         printf("\n");
     }
+    
+    return 0;
 }
